094b14db6045c848d067d0d3281d115d91b81da1
evgenGit81 <139344863+evgenGit81@users.noreply.github.com>
2024-04-29 04:28:22 +0700

    Update reboot_oper.py
    переработано

--- reboot_oper.py
+++ reboot_oper.py
@@ -1,20 +1,19 @@
 # -*- coding: utf-8 -*-
 class Building:
     """ Здание """
-    def __init__(self):
-        self.numberOfFloors = 100
-        self.buildingType = 'Башня'
 
-    def __eq__(self, y):
-        return self.numberOfFloors == y or self.buildingType == y
+    def __init__(self, numberOfFloors = 100, buildingType = "Башня"):
+        self.numberOfFloors = numberOfFloors
+        self.buildingType = buildingType
 
-
-c = ["Башня", 678, "Котетдж", 100, "Баня", 87]
-kl = Building()
-for i in range(len(c)):
-    kc = c[i]
-    print("True" if kl == kc else "False")
+    def __eq__(self, other):
+        return self.numberOfFloors == other.numberOfFloors and self.buildingType == other.buildingType
 
 
-
-
+builfr = {"Башня": 100, "Котетдж": 3, "Баня": 2}
+numfloors = Building()
+buildtype = Building()
+print(numfloors == Building() and buildtype == Building())
+for key in builfr.keys():
+    print(numfloors == Building(numberOfFloors=builfr[key]) and buildtype == Building(buildingType=key))
+    
